a0bfd08e60b3ad64bebb406c1b4b45c6ee33dbbe
RafalMalik <rafalmalik.info@gmail.com>
2020-04-02 20:41:49 +0200

    Created game model.

---
+++ src/Model/DTO/Game/Game.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Model\DTO\Game;
+
+class Game
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     */
+    private $description;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Game
+     */
+    public function setId(int $id): Game
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     * @return Game
+     */
+    public function setType(int $type): Game
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Game
+     */
+    public function setName(string $name): Game
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     * @return Game
+     */
+    public function setDescription(?string $description): Game
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
