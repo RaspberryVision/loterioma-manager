dd79899dba3957ec5961716697e202f5bd208cf2
RafalMalik <rafalmalik.info@gmail.com>
2020-09-29 21:20:21 +0200

    Added symfony messenger.

---
+++ src/Handler/UserRegistrationHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Handler;
+
+use App\Entity\User;
+use App\Message\UserRegistration;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class UserRegistrationHandler  implements MessageHandlerInterface
+{
+    /**
+     * @var EntityManagerInterface $entityManager
+     */
+    private $entityManager;
+
+    /**
+     * UserRegistrationHandler constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function __invoke(UserRegistration $message)
+    {
+        $user = json_decode($message->getContent());
+
+        $user1 = new User();
+        $user1->setEmail($user->id . '@cos@o2.pl')
+            ->setPassword($user->id);
+
+        $this->entityManager->persist($user1);
+        $this->entityManager->flush();
+    }
+}
