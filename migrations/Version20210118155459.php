e1ae086af166fca88d0f565dfe87d4e4c52f5c36
RafalMalik <rafalmalik.info@gmail.com>
2021-01-18 16:56:58 +0100

    Added missing migration file.

---
+++ migrations/Version20210118155459.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210118155459 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE slots_combination (id INT AUTO_INCREMENT NOT NULL, game_id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, fields LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', INDEX IDX_7988FE51E48FD905 (game_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE slots_combination ADD CONSTRAINT FK_7988FE51E48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE slots_combination');
+    }
+}
